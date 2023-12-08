0124ab433b292ccb37ce9927ae1fd6f1e3edfcaf
Casey Avila <casey@theavilas.org>
2023-12-08 01:13:29 -0800

    Add plus for portfolio

--- Frontend/src/pages/portfolio.js
+++ Frontend/src/pages/portfolio.js
@@ -63,6 +63,13 @@ export default function Portfolio() {
                 </div>
               </div>
             ))}
+            <Link legacyBehavior href="/create" passHref>
+              <a className="rounded-lg flex justify-center items-center border-4 border-dotted border-gray-300 hover:border-solid hover:bg-gray-50 transition-all duration-300 cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+            </svg>
+          </a>
+        </Link>
         </div>
       </div>
 
