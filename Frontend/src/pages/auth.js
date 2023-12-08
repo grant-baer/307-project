a249acf2849ead63c7856cabe557302e4cfa0890
Anshul Khandelwal <kanshul45@gmail.com>
2023-12-07 22:10:04 -0800

    git is stupid

---
+++ Frontend/src/pages/auth.js
@@ -0,0 +1,31 @@
+// auth.js (utility file)
+
+// Import the required libraries
+import axios from 'axios';
+
+// Function to check if the user is authenticated based on the backend verification
+export async function isAuthenticated(token) {
+  try {
+    // make axios get request sending cookie.
+    const response = await axios.get('http://localhost:5000/api/verify_user', {
+      headers: {
+        Authorization: 'Bearer ${token}', // Send the JWT token in the Authorization header
+      },
+    });
+
+    return response.data.authenticated;
+  } catch (error) {
+    // console.error('Error during user verification:', error);
+    return false;
+  }
+}
+const AuthPage = () => {
+  return (
+    <div>
+      <h1>You shouldn't have gotten here!</h1>
+      <p>This page is not meant to be accessed directly.</p>
+    </div>
+  );
+};
+
+export default AuthPage;
